c9080ba92a92314f62b82d44835fbc8130668e84
nikitanesterenkos@gmail.com <piramida741147>
2020-06-10 14:42:16 +0300

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,32 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+public class Main {
+    public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+        User user1 = new User("Ринат","Мансуров", (byte) 36);
+        User user2 = new User("Любовь","Мансурова", (byte) 35);
+        User user3 = new User("Аделина","Мансурова", (byte) 11);
+        User user4 = new User("Лилиана","Мансурова", (byte) 9);
+
+        userService.createUsersTable();
+
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        userService.removeUserById(3);
+        for (User element : userService.getAllUsers()) {
+            System.out.println(element);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+    }
+}
+
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,23 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/base";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
+
+    public static Connection ConnectionDB() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
